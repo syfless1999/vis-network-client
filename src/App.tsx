616a8a45d7e0d3e2cf323ec7db0622ef51ed54a9
syfless <syfless1999@gmail.com>
2020-12-08 12:24:44 +0800

    Init: add react, typescript, styled-components, d3

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from 'react';
+import styled from 'styled-components';
+import * as d3 from 'd3';
+
+const Header = styled.div`
+  text-align:center;
+`;
 
 function App() {
+  const header = useRef(null);
+  useEffect(() => {
+    d3.select(header.current)
+      .text('hello network visualization with React and D3');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header ref={header}>
+      ....
+    </Header>
   );
 }
 
