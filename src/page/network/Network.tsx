6ebe4e137a0b685cfe777309fcfeafc886dc4d0b
syfless <syfless1999@gmail.com>
2021-03-22 22:19:41 +0800

    Feat: network search around FIN

--- src/page/network/Network.tsx
+++ src/page/network/Network.tsx
@@ -8,20 +8,18 @@ import AsyncNodeMenu, { CompleteNetworkFunc } from 'src/page/component/GraphPane
 import Toolbar from 'src/page/component/GraphPanel/Toolbar';
 import LayoutSelector, { layouts } from 'src/page/component/GraphPanel/LayoutSelector';
 import SearchBar from 'src/page/component/SearchBar';
-import NodeMenu from 'src/page/component/GraphPanel/NodeMenu';
 import { completeNetworkData, getLayerNetworkData, getAroundNetwork } from 'src/service/network';
 import * as network from 'src/type/network';
 import { mergeTwoLayerNetwork, getDisplayLevelText, networkStyleWrapper } from 'src/util/network';
-import { array2Map } from 'src/util/array';
 
-import '@antv/graphin/dist/index.css';
+// import '@antv/graphin/dist/index.css';
 
-export interface NetworkParam {
+interface NetworkParam {
   label: string;
   taskId: string;
 }
 
-interface NetworkGraphState {
+interface NetworkState {
   layout: string;
   level: number;
   displayData: network.Network;
@@ -33,11 +31,9 @@ const {
 } = Behaviors;
 const { Option: SelectOption } = Select;
 
-const async = true;
-
 const Network = () => {
-  const [state, setState] = useState<NetworkGraphState>({
-    layout: 'random',
+  const [state, setState] = useState<NetworkState>({
+    layout: 'circle',
     level: -1,
     displayData: { nodes: [], edges: [] },
     sourceData: [],
@@ -47,16 +43,6 @@ const Network = () => {
   } = state;
   const maxLevel = sourceData.length - 1;
   const { label, taskId } = useParams<NetworkParam>();
-  // memo: node map
-  const nodeMap = useMemo(() => {
-    const nodes = sourceData.reduce<network.Node[]>((all, cur) => {
-      if (cur) {
-        return [...all, ...cur.nodes];
-      }
-      return all;
-    }, []);
-    return array2Map<string, network.Node>(nodes, (v) => v.id);
-  }, [sourceData]);
   // memo: layout type
   const layoutType = useMemo(() => layouts.find((l) => l.type === layout), [layout]);
   // memo: styled network data
@@ -77,31 +63,26 @@ const Network = () => {
     }));
   };
   const handleLevelChange = async (value: number) => {
-    if (value >= 0 && value <= maxLevel) {
-      let targetNetwork = sourceData[value];
-      let newSourceData = sourceData;
-      if (!targetNetwork) {
-        const expandData = await getLayerNetworkData({
-          taskId,
-          level: value.toString(),
-        });
-        newSourceData = mergeTwoLayerNetwork(sourceData, expandData);
-        targetNetwork = newSourceData[value];
-      }
-      setState((prev) => ({
-        ...prev,
-        level: value,
-        sourceData: newSourceData,
-        displayData: targetNetwork || prev.displayData,
-      }));
+    if (value < 0 || value > maxLevel) return;
+    let targetNetwork = sourceData[value];
+    let newSourceData = sourceData;
+    if (!targetNetwork) {
+      const expandData = await getLayerNetworkData({
+        taskId,
+        level: value.toString(),
+      });
+      newSourceData = mergeTwoLayerNetwork(sourceData, expandData);
+      targetNetwork = newSourceData[value];
     }
+    setState((prev) => ({
+      ...prev,
+      level: value,
+      sourceData: newSourceData,
+      displayData: targetNetwork || prev.displayData,
+    }));
   };
   const handleSearchNodeAroundNetwork = async (nodeId: string) => {
-    const newDisplayData = await getAroundNetwork({
-      label,
-      taskId,
-      nodeId,
-    });
+    const newDisplayData = await getAroundNetwork({ label, taskId, nodeId });
     handleDisplayDataChange(newDisplayData);
   };
   const completeNetwork: CompleteNetworkFunc = async (currentNetwork, newIds) => {
@@ -113,6 +94,7 @@ const Network = () => {
     });
     return newNetwork;
   };
+
   useEffect(() => {
     async function fetchData() {
       const data = await getLayerNetworkData({ taskId });
@@ -135,20 +117,17 @@ const Network = () => {
       >
         {/* 右键菜单：展开、收起 */}
         <ContextMenu>
-          {async ? (
-            <AsyncNodeMenu
-              displayData={displayData}
-              setDisplayData={handleDisplayDataChange}
-              completeNetwork={completeNetwork}
-            />
-          ) : (
-            <NodeMenu
-              sourceData={sourceData}
-              displayData={displayData}
-              setDisplayData={handleDisplayDataChange}
-              nodeMap={nodeMap}
-            />
-          )}
+          <AsyncNodeMenu
+            displayData={displayData}
+            setDisplayData={handleDisplayDataChange}
+            completeNetwork={completeNetwork}
+          />
+          {/* <NodeMenu
+            sourceData={sourceData}
+            displayData={displayData}
+            setDisplayData={handleDisplayDataChange}
+            nodeMap={nodeMap}
+          /> */}
         </ContextMenu>
         {/* 滚轮放大缩小：关闭 */}
         <ZoomCanvas disabled />
